7a9e8b787dd8b913e581e92001a5c467713e936b
exit <116209415+Z3ROCH13F@users.noreply.github.com>
2023-03-24 09:04:45 +0800

    update global/script.js

--- global/script.js
+++ global/script.js
@@ -1,18 +1,21 @@
 
-import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging";
+import { getMessaging, getToken } from "firebase/messaging";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-const firebaseConfig = {
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+const messaging = getMessaging();
+getToken(messaging, { vapidKey:
+'BKLUraCCqaKmA5A131v1d6-IOycLJDpKU_R55wr30qRtHh_NKoemVr-NM0kbCTvt3lsgXPAcdO69uDOetTDRl2w'
+}).then((currentToken) => {
+  if (currentToken) {
+    // Send the token to your server and update the UI if necessary
+    // ...
+  } else {
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    // ...
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
   // ...
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-// Initialize Firebase Cloud Messaging and get a reference to the service
-const messaging = getMessaging(app);
-
-alert(app,messaging);
+});
